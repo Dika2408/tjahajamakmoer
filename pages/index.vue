66a07c299157091f1ffb81d537ff72d514220db5
Dika2408 <141701838+Dika2408@users.noreply.github.com>
2024-09-25 08:56:29 +0700

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1>Tjahaja Makmoer</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <p><h3>caffeteria</h3></p>
+          <p>Senin-Kamis, Sabtu 11:00-02.00 WIB</p>
+          <p>Jumat 13:00-02:00 WIB</p>
+          <p>Minggu 09:00-02.00 WIB</p>
+          <p>WAROENG KOPI</p>
+          <p>#WarkopTjahajaMakmoer</p>
+          <p>Purbaratu - Tasikmalaya</p>
+          <p>WARKOP TJAHAJA MAKMOER, PURBARATU, TASIKMALAYA 46196</p>
+        </div>
+      </div>
+    </div>
+  </template>
