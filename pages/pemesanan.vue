15f6089006a7d9a224b3f5db241671fee97b2124
Dika2408 <141701838+Dika2408@users.noreply.github.com>
2024-10-07 10:39:50 +0700

    Add files via upload

---
+++ pages/pemesanan.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Hot Coffee</h1>
+    <h1>Cold Coffee</h1>
+    <h1>Others</h1>
+
+    <ul v-for="item in items" :key="item.id">
+      <li>{{ item.nama }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const supabase = useSupabaseClient()
+const items = ref([])
+
+async function getProduk() {
+  let { data, error } = await supabase
+  .from('produk')
+  .select('*')
+  if(data) {
+    items.value = data
+  }
+}
+
+onMounted(() => getProduk())
+</script>
